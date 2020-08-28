79c5de9139ce8710075192cc231e95b9a564d8e7
Anton Shtipliyski <antonshtipliyski@yahoo.com>
2020-08-27 21:25:35 -0300

    Fetching data from Shopify backend. Displaying it on home page.

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { Provider as StyletronProvider, DebugEngine } from "styletron-react";
 import { Client as Styletron } from "styletron-engine-atomic";
-import Home from '../pages/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ShopProvider from '../context/shopContext';
+import Home from '../pages/Home';
+import Product from '../pages/Product';
+
 
 const debug =
   process.env.NODE_ENV === "production" ? void 0 : new DebugEngine();
@@ -11,9 +14,20 @@ const engine = new Styletron();
 
 function App() {
   return (
-    <StyletronProvider value={engine} debug={debug} debugAfterHydration>
-      
-    </StyletronProvider>
+    <ShopProvider>
+      <StyletronProvider value={engine} debug={debug} debugAfterHydration>
+        <Router>
+          <Switch>
+            <Route path='/product/:id'>
+              <Product />
+            </Route>
+            <Route path='/'>
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
+      </StyletronProvider>
+    </ShopProvider>
   );
 }
 
